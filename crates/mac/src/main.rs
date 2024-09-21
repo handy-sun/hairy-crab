3573038ec5db31b42bb0af3502e2e91243cd1e03
sooncheer <handy-sun@foxmail.com>
2024-09-21 23:20:16 +0800

    fix windows cmd 'date'

--- crates/mac/src/main.rs
+++ crates/mac/src/main.rs
@@ -6,6 +6,10 @@ use std::str::{from_utf8, Utf8Error};
 const SHORT_COMMIT: &[u8] = cmd_execute!("git rev-parse --short HEAD").trim_ascii_end(); // git rev-parse --short HEAD | tr -d '\n\r'
 
 const RES: Result<&str, Utf8Error> = from_utf8(SHORT_COMMIT);
+
+#[cfg(target_os="windows")]
+const BUILD_TIME: &[u8] = cmd_execute!("echo %date:~0,10% %time:~0,8%").trim_ascii_end();
+#[cfg(not(target_os="windows"))]
 const BUILD_TIME: &[u8] = cmd_execute!("date '+%Y%m%d %H:%M:%S %:z'").trim_ascii_end();
 
 // const COMMIT_TIME: &[u8] = cmd_execute!("git log -1 --format=%cd");
