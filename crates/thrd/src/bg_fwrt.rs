3791e15748e9781e3b88d2a43dca474577edd644
sooncheer <handy-sun@foxmail.com>
2024-09-22 20:19:48 +0800

    add mod bg_fwrt

---
+++ crates/thrd/src/bg_fwrt.rs
@@ -0,0 +1,55 @@
+use std::fs::File;
+use std::io::Write;
+use std::sync::mpsc::{self, Sender};
+use std::thread::{self, JoinHandle};
+
+pub type BoxStrRE = Result<BgFileWriter, Box<str>>;
+// type StaticStrRE = Result<BgFileWriter, &'static str>;
+
+/// Start a background thread to write data into file
+#[allow(dead_code)]
+pub struct BgFileWriter {
+    transmitte: Sender<String>,
+    // rx: Receiver<T>,
+    bg_thrd: JoinHandle<()>,
+    // out_file: &'a mut File,
+}
+
+impl BgFileWriter {
+    pub fn init(file_name: &str) -> BoxStrRE {
+        let (tx, rx) = mpsc::channel::<String>();
+
+        let handle = thread::Builder::new()
+            .name("bg-file-writer-thread".to_string())
+            .spawn({
+                let mut file = File::create(file_name)
+                    .map_err(|err| format!("Could not create file : {}", err).into_boxed_str())?;
+                move || {
+                    for msg in &rx {
+                        file.write_all(msg.as_bytes()).unwrap_or_else(|op| eprint!("op={}", op));
+                    }
+                }
+            })
+            .map_err(|err| format!("Thread error: {}", err).into_boxed_str())?;
+
+        Ok(Self {
+            transmitte: tx,
+            bg_thrd: handle,
+        })
+    }
+
+    #[allow(dead_code)]
+    pub fn send_bytes(&mut self, str_data: &[u8]) -> Result<(), mpsc::SendError<String>> {
+        // if let Err(err) = self.transmitte.send(String::from_utf8_lossy(str_data).to_string()) {
+        //     return Err(anyhow!("SendError: {:?}", err));
+        // }
+        // Ok(())
+        self.transmitte
+            .send(String::from_utf8_lossy(str_data).to_string())
+    }
+
+    #[allow(dead_code)]
+    pub fn is_finished(&self) -> bool {
+        self.bg_thrd.is_finished()
+    }
+}
--- crates/thrd/src/main.rs
+++ crates/thrd/src/main.rs
@@ -1,67 +1,23 @@
-// use std::env;
+mod bg_fwrt;
 
-use std::fs::File;
-use std::io::{self, Write};
-use std::thread::{self, ThreadId};
-
-use std::sync::mpsc;
-
-use chrono::{Local, Timelike};
-
+use std::thread;
 use std::time::Duration;
 
-const TEST_FILE: &str = "/tmp/output.txt";
+use chrono::Local;
 
-const THOU_MILL: Duration = Duration::from_millis(500);
+use crate::bg_fwrt::BgFileWriter;
 
-fn write_to_file(
-    thrd_id: &ThreadId,
-    filename: &str,
-    messages: mpsc::Receiver<String>,
-) -> io::Result<()> {
-    let mut file = File::create(filename).expect("Could not create file");
-    for msg in messages {
+const TEST_FILE: &str = "/tmp/bg_fwrt.txt";
 
-        let now = Local::now().time().nanosecond();
-        let str_data = format!(
-            "{:?}: {}{:?}: <recv time: {:10}>\n",
-            thrd_id,
-            msg,
-            thread::current().id(),
-            // now.timestamp_micros()
-            now
-        );
-        file.write_all(str_data.as_bytes())?;
+const HALF_SEC: Duration = Duration::from_millis(500);
+
+fn main() -> Result<(), Box<str>> {
+    let mut bgw = BgFileWriter::init(TEST_FILE)?;
+    for _ in 0..5 {
+        bgw.send_bytes(format!("{}\n", Local::now()).as_bytes())
+            .unwrap_or_else(|op| eprintln!("Send Bytes: {}", op));
+        thread::sleep(HALF_SEC);
     }
-    file.write_all(b"step\n")
-}
-
-fn main() -> io::Result<()> {
-    let (tx0, rx0) = mpsc::channel();
-    // let (tx1, rx1) = mpsc::channel();
-
-    let handle1 = thread::spawn(move || {
-        for _ in 0..10 {
-            let now = Local::now().time().nanosecond();
-            tx0.send(format!("<send time: {:10}>\n", now)).unwrap_or_default();
-            thread::sleep(THOU_MILL);
-        }
-    });
-
-    let h1 = handle1.thread().id();
-    let handle2 = thread::spawn(move || match write_to_file(&h1, TEST_FILE, rx0) {
-        Err(_err) => {
-            eprintln!("Error: {}", _err);
-            std::process::exit(1);
-        }
-        _ => {}
-    });
-    // dbg!(&handle1.thread().id());
-    // dbg!(&handle2.thread().id());
-    handle1.join().unwrap_or_default();
-    handle2.join().unwrap_or_default();
-    // drop(tx0);
-    // drop(tx1);
-    println!("main: {:?}", thread::current().id());
-    Ok(())
+    bgw.send_bytes(b"finish\n")
+        .map_err(|op| format!("Send Bytes: {}", op).into_boxed_str())
 }
