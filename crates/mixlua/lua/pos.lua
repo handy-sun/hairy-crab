8798715d95138d9a490c60e6ab4a1d470a416574
sooncheer <handy-sun@foxmail.com>
2024-10-14 16:37:14 +0800

    conver simple bytes to structure data

--- crates/mixlua/lua/pos.lua
+++ crates/mixlua/lua/pos.lua
@@ -1,27 +1,66 @@
-Position = {}
-Position.__index = Position
 
-function Position.new(x, y)
-    return setmetatable({ x = x, y = y }, Position)
+-- Convert binary(string) to number
+function Bytes2int(bytes, is_little_endian)
+    local fmt = (is_little_endian == true and "<I" or ">I") .. #bytes
+    return string.unpack(fmt, bytes)
 end
 
-function Position:add(other)
-    return Position.new(self.x + other.x, self.y + other.y)
+function SetupTableData(bytes, tab_list)
+    local index = 1
+    local new_list = {}
+    for _, value in pairs(tab_list) do
+        if index > #bytes then
+            break
+        end
+        local part_size = value.size
+        local next_index = index + part_size
+        local part_bytes = string.sub(bytes, index, next_index - 1)
+
+        table.insert(new_list, {
+            name = value.name,
+            size = value.size,
+            data = Bytes2int(part_bytes, true)
+        })
+
+        index = next_index
+    end
+    return new_list
+end
+
+function PrintTable(t, indent)
+    indent = indent or 0
+    local prefix = string.rep("  ", indent)
+    local str = ""
+    for key, value in pairs(t) do
+        if type(value) == "table" then
+            str = str .. (prefix .. tostring(key) .. ':\n')
+            str = str .. PrintTable(value, indent + 1)
+        else
+            str = str .. (prefix .. tostring(key) .. ": " .. tostring(value) .. '\n')
+        end
+    end
+    return str
+end
+
+Structure = {}
+Structure.__index = Structure
+
+function Structure.new(bytes)
+    local _temp = {
+        { name = "index", size = 4 },
+        { name = "byte", size = 1 },
+        { name = "uns", size = 2 },
+    }
+
+    return setmetatable({ inner = SetupTableData(bytes, _temp) }, Structure)
 end
 
 -- Overload `__tostring` meta method
-function Position:__tostring()
-    return "Position(" .. self.x .. ", " .. self.y .. ")"
+function Structure:__tostring()
+    return PrintTable(self.inner)
 end
 
-local pos_a = Position.new(1, 2)
-local pos_b = Position.new(5, 3)
+-- local bytes = "\x04\x00\x01\x00\x0b\x03\x01\x0a"
+-- local test_stru = Structure.new(bytes)
 
-local pos_add = pos_a:add(pos_b)
-print(pos_add)
-
--- Test: convert binary(string) to number
-local bin = "\x12\xef"
-print("bin.len: " .. string.len(bin))
-local num = string.byte(bin, 2) | (string.byte(bin, 1) << 8)
-print(string.format("%#x", num))
+-- print(test_stru)
--- crates/mixlua/src/main.rs
+++ crates/mixlua/src/main.rs
@@ -1,54 +1,58 @@
-use mlua::{Lua, ObjectLike, Table};
+
+use mlua::{Lua, ObjectLike, Table, Value};
+
+fn print_table<'a>(tab: &'a Table, indent: usize) -> mlua::Result<String> {
+    let prefix = vec!["  "; indent].concat();
+    let mut output = String::with_capacity(64);
+    for pair in tab.pairs::<Value, Value>() {
+        let (key, value) = pair?;
+        let key_str = match key {
+            Value::Integer(i) => i.to_string(),
+            Value::String(s) => s.to_string_lossy().to_string(),
+            _ => String::from("nil"),
+        };
+
+        match value {
+            Value::Table(child) => {
+                output.push_str(format!("{}{:?}:\n", prefix, key_str).as_ref());
+                output.push_str(&print_table(&child, indent + 1)?);
+            }
+            Value::Integer(integer) => output.push_str(format!("{}{:?}: {}\n", prefix, key_str, integer).as_ref()),
+            Value::String(s) => output.push_str(format!("{}{:?}: {}\n", prefix, key_str, s.to_string_lossy()).as_ref()),
+            _ => ()
+        }
+    }
+    return Ok(output);
+}
 
 fn main() -> mlua::Result<()> {
+    let Some(lua_file) = std::env::args().skip(1).next() else {
+        eprintln!("Must input lua file");
+        std::process::exit(1);
+    };
+
+    if !std::fs::exists(&lua_file)? {
+        eprintln!("Lua file not exist");
+        std::process::exit(1);
+    }
+
     // This loads the default Lua std library *without* the debug library.
     let lua_ctx = Lua::new();
     let globals = lua_ctx.globals();
-
-    // globals.set("string_var", "hello")?;
-    // globals.set("int_var", 42)?;
-
-    // assert_eq!(globals.get::<String>("string_var")?, "hello");
-    // assert_eq!(globals.get::<i64>("int_var")?, 42);
-
-    lua_ctx
-        .load(
-            r#"
-Position = {}
-Position.__index = Position
-
-function Position.new(x, y)
-    return setmetatable({ x = x, y = y }, Position)
-end
-
-function Position:add(other)
-    return Position.new(self.x + other.x, self.y + other.y)
-end
-
--- Overload `__tostring` meta method
-function Position:__tostring()
-    return "Position(" .. self.x .. ", " .. self.y .. ")"
-end
-"#,
-        )
-        .set_name("example code")
-        .exec()?;
-
-    let pos_a = lua_ctx.create_table()?;
-    pos_a.set("x", 1i32)?;
-    pos_a.set("y", 2i32)?;
-
-    // let pos_b = lua_ctx.create_table()?;
-    // pos_b.set("x", 3i32)?;
-    // pos_b.set("y", 4i32)?;
+    let file_content = std::fs::read(&lua_file)?;
+    lua_ctx.load(file_content.to_vec()).exec()?;
 
     // if pos_b want call method must create by globals
-    let pos_b: Table = globals
-        .get::<Table>("Position")?
-        .call_function("new", (3i32, 4i32))?;
+    let bytes = "\x04\x00\x01\x00\x0b\x03\x01\x0a";
 
-    let pos_sum: Table = pos_b.call_method("add", pos_a)?;
-    println!("Lua structure sum result: {:?}", pos_sum.to_string()?);
+    let res: Table = globals
+        .get::<Table>("Structure")?
+        .call_function("new", bytes)?;
+
+    println!("result: {:?}", res.to_string()?);
+
+    let inner: Table = res.get("inner")?;
+    println!("{}", print_table(&inner, 0)?);
 
     Ok(())
 }
