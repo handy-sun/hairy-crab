e4a220e6121ca56cddcb4f56fdc38d95ec4fecec
sooncheer <handy-sun@foxmail.com>
2024-11-01 10:26:05 +0800

    use walkdir to ergodic lua files

--- crates/mixlua/src/main.rs
+++ crates/mixlua/src/main.rs
@@ -1,7 +1,8 @@
-use mlua::{Lua, ObjectLike, Table, Value};
 use std::{fs, env};
+use mlua::{Lua, ObjectLike, Table, Value};
+use walkdir::WalkDir;
 
-
+#[allow(dead_code)]
 fn print_table(tab: &Table, indent: usize) -> mlua::Result<String> {
     let prefix = vec!["  "; indent].concat();
     let mut output: Vec<_> = Vec::with_capacity(16);
@@ -49,29 +50,37 @@ fn main() -> mlua::Result<()> {
     let lua = Lua::new();
     let globals = lua.globals();
 
-    let files = vec!["init.lua", "st.lua"];
-    for f in files {
-        let lua_src_dir = self_dir.join(f);
-        if !fs::exists(&lua_src_dir)? {
-            eprintln!("Lua file not exist: {:?}", lua_src_dir);
-            std::process::exit(1);
+    let walker = WalkDir::new(self_dir).max_depth(1).follow_links(true).into_iter();
+    for res_entry in walker {
+        if let Ok(entry) = res_entry {
+            if entry.path().is_dir() {
+                continue;
+            }
+            println!("{:?}", entry.path());
+            let file_content = fs::read_to_string(entry.path())?;
+            lua.load(&file_content).exec()?;
         }
-
-        let file_content = fs::read_to_string(&lua_src_dir)?;
-        lua.load(&file_content).exec()?;
     }
 
     let lua_str = lua.create_string(b"\x04\x00\x00\x00\x0b\x03\xa1\x0a")?;
 
-    let res: Table = globals
+    let _res: Table = globals
         .get::<Table>("Structure")?
         .call_function("new_inner", lua_str)?;
-    // println!("result: {:?}", res.to_string()?);
 
-    let inner: Table = res.get("inner")?;
+    let inner: Table = _res.get("inner")?;
     println!("{}", print_table(&inner, 0)?);
 
-    // let package: Table = globals.get("package").context("get _G.package")?;
-    // println!("{}", print_table(&package, 0)?);
+    let package: Table = globals.get("package")?;
+    let path: mlua::String = package.get("path")?;
+    println!("package.path: {}", path.to_str()?);
+
+    let c_path: mlua::String = package.get("cpath")?;
+    println!("package.cpath: {}", c_path.to_str()?);
+
+    // let loaded = package.get::<Table>("loaded")?;
+    // println!("{:?}", loaded);
+    // let pair = globals.call_function("pairs", loaded)?;
+    // println!("pairs of package.loaded: {}", print_table(&pair, 0)?);
     Ok(())
 }
